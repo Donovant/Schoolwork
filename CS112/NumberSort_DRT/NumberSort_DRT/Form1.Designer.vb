18fa10360faf6201ab3ca344a551d1b722fb903a
Donovant <Donovan@torgersonlabs.com>
2017-11-07 22:54:47 -0700

    Initial commits of NumberSort and Prog1 VisualBasic programs.
    Initial commits of NumberSort and Prog1 VisualBasic programs.

---
+++ CS112/NumberSort_DRT/NumberSort_DRT/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstInput = New System.Windows.Forms.ListBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.lstOutput = New System.Windows.Forms.ListBox
+        Me.SuspendLayout()
+        '
+        'lstInput
+        '
+        Me.lstInput.FormattingEnabled = True
+        Me.lstInput.Location = New System.Drawing.Point(17, 22)
+        Me.lstInput.Name = "lstInput"
+        Me.lstInput.Size = New System.Drawing.Size(120, 381)
+        Me.lstInput.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(162, 201)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Sort"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.Location = New System.Drawing.Point(262, 22)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(120, 381)
+        Me.lstOutput.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(398, 425)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lstInput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lstInput As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents lstOutput As System.Windows.Forms.ListBox
+
+End Class
