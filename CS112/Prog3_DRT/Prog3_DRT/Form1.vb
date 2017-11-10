a47d34fa045bad0c75b06d0f82d4986e12b8c4ec
Donovant <Donovan@torgersonlabs.com>
2017-11-09 21:35:34 -0700

    Initial Commit of Prog3

---
+++ CS112/Prog3_DRT/Prog3_DRT/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim hrsCycle As Double = 200
+        Dim hrsRun As Double = 475
+        Dim hrsSwim As Double = 275
+        Dim cycleBox As Double
+        Dim runBox As Double
+        Dim swimBox As Double
+        Dim totalCycle As Double
+        Dim totalRun As Double
+        Dim totalSwim As Double
+        Dim totalOutput As Double
+        Dim weightLoss As Double
+        cycleBox = CDbl(Cycling.Text)
+        runBox = CDbl(Running.Text)
+        swimBox = CDbl(Swimming.Text)
+        totalCycle = hrsCycle * cycleBox
+        totalRun = hrsRun * runBox
+        totalSwim = hrsSwim * swimBox
+        totalOutput = totalCycle + totalRun + totalSwim
+        weightLoss = totalOutput / 3500
+        Output.Text = FormatNumber(weightLoss, 2) & " pounds were lost"
+    End Sub
+End Class
---
+++ CS112/Prog3_DRT/readme.md
@@ -0,0 +1,2 @@
+# Assignment 3:
+** This program currently will not compile. I am in the process of trying to find the assignment details to recreate this project.
