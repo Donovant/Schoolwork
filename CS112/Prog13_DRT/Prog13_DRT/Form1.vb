90cd9867b97de109782f26362b08d29636d6f54d
Donovant <Donovan@torgersonlabs.com>
2017-11-13 19:40:42 -0700

    Initial Commit of Prog13 & Prog15 (no readme.md files)

---
+++ CS112/Prog13_DRT/Prog13_DRT/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim fmtStr As String = "{0,-25}{1,15}"
+        lstOutput.Items.Add(String.Format(fmtStr, "College", "Percent of Change"))
+        getInput()
+
+    End Sub
+    Sub getInput()
+        Dim College As String
+        Dim tf0607, tf0708 As Double
+        Dim sr As IO.StreamReader = IO.File.OpenText("Colleges.txt")
+        College = sr.ReadLine
+        tf0607 = CDbl(sr.ReadLine)
+        tf0708 = CDbl(sr.ReadLine)
+        showOutput(College, Change(tf0607, tf0708))
+        College = sr.ReadLine
+        tf0607 = CDbl(sr.ReadLine)
+        tf0708 = CDbl(sr.ReadLine)
+        showOutput(College, Change(tf0607, tf0708))
+        College = sr.ReadLine
+        tf0607 = CDbl(sr.ReadLine)
+        tf0708 = CDbl(sr.ReadLine)
+        showOutput(College, Change(tf0607, tf0708))
+        College = sr.ReadLine
+        tf0607 = CDbl(sr.ReadLine)
+        tf0708 = CDbl(sr.ReadLine)
+        showOutput(College, Change(tf0607, tf0708))
+        sr.Close()
+    End Sub
+    Sub showOutput(ByVal College As String, ByVal percentage As Double)
+        Dim txt1, txt2 As String
+        Dim fmtStr As String = "{0,-25}{1,15:p}"
+
+        lstOutput.Items.Add(String.Format(fmtStr, College, percentage))
+    End Sub
+
+    Function Change(ByVal tf0607 As Double, ByVal tf0708 As Double)
+        Dim percentage As Double
+        percentage = ((tf0708 - tf0607) / tf0607)
+        Return percentage
+    End Function
+End Class
---
+++ CS112/Prog15_DRT/Prog15_DRT/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Function CalcConv(ByVal tmpCel As Double)
+        Dim Far As Double
+        Far = (9 / 5) * tmpCel + 32
+        Return Far
+
+    End Function
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim tmpCel As Double = -40
+        Dim tmpFar As Double
+        Dim fmtStr As String = "{0, -15}{1, -15}"
+        lstOutput.Items.Add(String.Format(fmtStr, "Celcius", "Fahrenheit"))
+        Do While (tmpCel >= -40) And (tmpCel <= 40)
+            lstOutput.Items.Add(String.Format(fmtStr, tmpCel, CalcConv(tmpCel)))
+            tmpCel += 5
+        Loop
+    End Sub
+End Class
