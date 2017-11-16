b7c78c3a3b59b9cd30a3cbc1baf8fab1ea803057
Donovant <Donovan@torgersonlabs.com>
2017-11-16 02:06:50 -0700

    Initial Commit of Prog17 (No readme.md file)

---
+++ CS112/Prog17_DRT/Prog17_DRT/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub btnCalculate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalculate.Click
+        Dim num, smallest, largest As Double
+        Dim sr As IO.StreamReader = IO.File.OpenText("numbers.txt")
+        num = sr.ReadLine
+        lstNumbers.Items.Add(num)
+        num = smallest
+        num = largest
+        Do While sr.Peek <> -1
+            num = sr.ReadLine
+            lstNumbers.Items.Add(num)
+            If num < smallest Then
+                smallest = num
+            End If
+            If num > largest Then
+                largest = num
+            End If
+        Loop
+        sr.Close()
+        txtSmall.Text = smallest
+        txtLarge.Text = largest
+    End Sub
+End Class
