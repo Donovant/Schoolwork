3cbf9c102b5fa2391a0f3373e8a4729a952a66d9
Donovant <Donovan@torgersonlabs.com>
2017-11-16 01:56:23 -0700

    Initial Commit of Prog16 (No readme.md file)

---
+++ CS112/Prog16_DRT/Prog16_DRT/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstOutput = New System.Windows.Forms.ListBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.txtYr = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'lstOutput
+        '
+        Me.lstOutput.Font = New System.Drawing.Font("Lucida Console", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.ItemHeight = 12
+        Me.lstOutput.Location = New System.Drawing.Point(11, 12)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(217, 172)
+        Me.lstOutput.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(82, 230)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtYr
+        '
+        Me.txtYr.Font = New System.Drawing.Font("Lucida Console", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtYr.Location = New System.Drawing.Point(69, 201)
+        Me.txtYr.Name = "txtYr"
+        Me.txtYr.ReadOnly = True
+        Me.txtYr.Size = New System.Drawing.Size(100, 19)
+        Me.txtYr.TabIndex = 2
+        Me.txtYr.TabStop = False
+        Me.txtYr.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(238, 262)
+        Me.Controls.Add(Me.txtYr)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Name = "Form1"
+        Me.Text = "World Population"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lstOutput As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtYr As System.Windows.Forms.TextBox
+
+End Class
---
+++ CS112/Prog16_DRT/Prog16_DRT/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim year As Double = 2039
+        Dim pop As Double = 12000000000
+        Dim fmtStr As String = "{0, -10}{1, -20}"
+        lstOutput.Items.Clear()
+        lstOutput.Items.Add(String.Format(fmtStr, "Year", "Population"))
+        Do While pop > 6000000
+            year = (year - 40)
+            pop = (pop / 2)
+            lstOutput.Items.Add(String.Format(fmtStr, year, FormatNumber(pop, 0)))
+        Loop
+        txtYr.Text = year
+    End Sub
+    Function CalcYear(ByVal prevYear As Double)
+        prevYear -= 40
+        Return prevYear
+    End Function
+    Function CalcPop(ByVal newPop As Double)
+        newPop = newPop / 2
+        Return newPop
+    End Function
+
+End Class
