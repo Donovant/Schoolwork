4dffffe5db803d3d4e52ed3fe44ea4d676f14b42
Donovant <Donovan@torgersonlabs.com>
2017-11-16 22:00:34 -0700

    Initial Commit of Prog19

---
+++ CS112/Prog19_DRT/Prog19_DRT/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstOutput = New System.Windows.Forms.ListBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.Location = New System.Drawing.Point(122, 14)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(120, 199)
+        Me.lstOutput.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 102)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(254, 226)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Name = "Form1"
+        Me.Text = "Perfect Number Finder"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lstOutput As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ CS112/Prog19_DRT/Prog19_DRT/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim coefficient, max As Integer
+        Dim number As Integer = 0
+        For num As Integer = 1 To 10000
+            For divisor As Integer = 1 To num - 1
+                If (num Mod divisor = 0) Then
+                    number += divisor
+                End If
+            Next
+            If number = num Then
+                lstOutput.Items.Add(number)
+            End If
+            number = 0
+        Next
+    End Sub
+End Class
